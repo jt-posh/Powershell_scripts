c12e70b26895b5b94c508c51f978d52633e31642
jt-posh <jantone86@yahoo.com>
2019-08-11 11:04:32 -0500

    Signed-off-by: jt-posh <jantone86@yahoo.com>

---
+++ Get-OperatingSystem.ps1
@@ -0,0 +1,5 @@
+﻿### Get-OperatingSystem.ps1 ###
+Param($ComputerName = "LocalHost")
+$cmd=gci -Path 'hklm:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\'
+$cmd
+#Invoke-Command -cn $ComputerName -ScriptBlock {$cmd}
