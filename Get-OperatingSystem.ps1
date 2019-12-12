f0dd6759fd1b33f557b4f561edaf80525884ddfa
jt-posh <53841340+jt-posh@users.noreply.github.com>
2019-12-12 12:16:06 -0600

    Delete Get-OperatingSystem.ps1

--- Get-OperatingSystem.ps1
+++
@@ -1,5 +0,0 @@
-﻿### Get-OperatingSystem.ps1 ###
-Param($ComputerName = "LocalHost")
-$cmd=gci -Path 'hklm:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\'
-$cmd
-#Invoke-Command -cn $ComputerName -ScriptBlock {$cmd}
