b28bebd15aa00737582706358780799147897173
jt-posh <jantone86@yahoo.com>
2020-03-15 09:42:01 -0500

    Signed-off-by: jt-posh <jantone86@yahoo.com>

---
+++ secedit-parse.ps1
@@ -0,0 +1,28 @@
+﻿SecEdit.exe /export /areas user_rights /cfg C:\temp\sec.txt
+$sec = Get-Content C:\temp\sec.txt
+$out = @()
+$record = @()
+$hostname = HOSTNAME.EXE
+$timestamp = Get-Date -Format o
+foreach ($line in $sec) {
+    if (($line[0] -eq "[") -or ($line -match "signature") -or ($line -match "Revision")-or ($line -match "Unicode")){
+        continue
+    }
+    $split = $line -split "="
+    $key = $split[0]
+    $value = $split[1]
+    $valuesplit = $value -split ","
+ #       for ($x = 0; $x -lt $valuesplit.count; $x++){
+ #           "val$x" = $valuesplit[$x]
+ #       }
+    $record = New-Object psobject -Property @{
+        "key" = $key
+        "value" = $value
+        "@timestamp" = $timestamp
+        "host.hostname" = $hostname
+    }
+    $out += $record
+
+}
+$out | ConvertTo-Json
+Remove-Item c:\temp\sec.txt
